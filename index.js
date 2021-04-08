f6695e140948f80b9c4934760526370d2f6fb0a9
Fabrice DAO <dao.fabrice@gmail.com>
2021-04-09 01:39:56 +0200

    2ème version, fichier 3 et 5 en echecs

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-console.log('Hello Weglot !');
 
-import { parseFileIntoWeeklyObject, sortWeekByAscStartTime, writeResult } from './src/fileParser.js';
+
+import { parseFileIntoWeeklyObject, reduceIntersecWeek,sortWeekByAscStartTime, writeResult } from './src/fileParser.js';
 import { findTimeSlot } from './src/findTimeSlot.js';
 import { convertObjectToTimeSlot } from "./src/timeConverter.js";
+import {  createInterface } from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 let indexFile = 1;
 
-const name = prompt('What is your name?');
-console.log(`Hey there ${name}`);
+rl.question('Witch input file do you wanna read ? (number between 1 and 5 expected...)\n', (answer) => {
+    console.warn(`If output${answer}.txt already exist in the data/generated folder, your PC will explose... Mouhahahahaha...`);
+    indexFile = Number(answer);
 
-parseFileIntoWeeklyObject(indexFile).then(data => {
+    parseFileIntoWeeklyObject(indexFile).then(data => {
 
-    let parsedWeek = sortWeekByAscStartTime(data);
-    let timeSlot = findTimeSlot(parsedWeek);
+        console.log(data);
 
-    console.log(timeSlot.foundDay + ' ' + convertObjectToTimeSlot(timeSlot.foundTimeSlot))
-    writeResult(indexFile, timeSlot.foundDay + ' ' + convertObjectToTimeSlot(timeSlot.foundTimeSlot));
+        let parsedWeek = reduceIntersecWeek(data);
+        let timeSlot = findTimeSlot(parsedWeek);
+
+        writeResult(indexFile, timeSlot.foundDay + ' ' + convertObjectToTimeSlot(timeSlot.foundTimeSlot))
+        .then(() =>{
+            console.log(timeSlot.foundDay + ' ' + convertObjectToTimeSlot(timeSlot.foundTimeSlot))
+        });
+    });
+
+    rl.close();
 });
+
+
+
